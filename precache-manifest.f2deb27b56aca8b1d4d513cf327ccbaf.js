dbc774ddde911d58d5eeb1b6f8e6a42d9ffa5c63
Evgenia Rangaev <alraner93@gmail.com>
2020-10-10 19:30:18 -0400

    Updates

---
+++ precache-manifest.f2deb27b56aca8b1d4d513cf327ccbaf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d56da2b1a2d69df0bf24e117c871f77b",
+    "url": "/jeopardy_game/index.html"
+  },
+  {
+    "revision": "b80cc8bf7354bd82155b",
+    "url": "/jeopardy_game/static/css/main.d32db3e3.chunk.css"
+  },
+  {
+    "revision": "82bba012738e0ae2dab5",
+    "url": "/jeopardy_game/static/js/2.52b84bc7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/jeopardy_game/static/js/2.52b84bc7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b80cc8bf7354bd82155b",
+    "url": "/jeopardy_game/static/js/main.7df78def.chunk.js"
+  },
+  {
+    "revision": "dc6988dd9477be932e2a",
+    "url": "/jeopardy_game/static/js/runtime-main.ea85a810.js"
+  },
+  {
+    "revision": "0fa69445c4d37a9d86d0f72f21e8a221",
+    "url": "/jeopardy_game/static/media/nothingToSee.0fa69445.png"
+  }
+]);
